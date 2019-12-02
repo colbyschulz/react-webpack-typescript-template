e40571ab81640158ef5f704879da411b3abaa75b
Colby Schulz <colbyschulz@Colbys-MacBook-Pro.local>
2019-12-02 17:52:00 -0500

    configue eslint and prettier

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-const App = () => {
-  return (
-    <div>Hello World</div>
-  );
-}
+const App: React.FunctionComponent = () => {
+  return <div>Hello World</div>;
+};
 
 export default App;
