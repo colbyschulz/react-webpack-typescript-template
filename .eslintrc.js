c1ffd0a116548ec69f6150d79016d74c13225248
Colby Schulz <colbyschulz@Colbys-MacBook-Pro.local>
2021-05-10 11:06:18 -0400

    update prettier settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,7 @@ module.exports =  {
   extends:  [
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    "prettier",
   ],
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
